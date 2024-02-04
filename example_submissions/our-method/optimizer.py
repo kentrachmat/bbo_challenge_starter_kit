f6a209b8113658e21c0770f5cd860f6dbaca1348
Diego Torres <diegotg2000@hotmail.com>
2024-02-04 18:12:04 +0100

    errors fixed

--- example_submissions/our-method/optimizer.py
+++ example_submissions/our-method/optimizer.py
@@ -27,23 +27,30 @@ class OurOptimizer(AbstractOptimizer):
         self.random = random
         self.meta_model = GaussianProcessRegressor(kernel=Matern(nu=2.5))
         self.api_config = api_config
-        self.preprocessor = self.build_preprocessor(api_config)
+        self.preprocessor_dict = self.build_preprocessor(api_config)
+
+    def process(self, x):
+        features = []
+        for feature in self.api_config:
+            feature_values = x[feature]
+            features.append(self.preprocessor_dict[feature](feature_values))
+        return features
 
     def build_preprocessor(self, api_config):
         processing_dict = {}
         for feature in api_config:
-            if api_config[feature] == 'bool':
+            if api_config[feature]['type'] == 'bool':
                 processing_dict[feature] = lambda x: int(x)
 
-            elif api_config[feature] == 'cat':
+            elif api_config[feature]['type'] == 'cat':
                 processing_dict[feature] = self.categorical_processing(api_config[feature]['values'])
 
-            elif api_config[feature] == 'int':
+            elif api_config[feature]['type'] == 'int':
                 if 'range' in api_config[feature]:
                     min_val, max_val = api_config[feature]['range']
                     processing_dict[feature] =  self.min_max_processing(min_val, max_val)
 
-            elif api_config[feature] == 'real':
+            elif api_config[feature]['type'] == 'real':
                 if 'range' in api_config[feature]:
                     min_val, max_val = api_config[feature]['range']
                     if api_config[feature]['space'] == 'log':
@@ -52,6 +59,9 @@ class OurOptimizer(AbstractOptimizer):
                         processing_dict[feature] =  lambda x: self.min_max_processing(min_val, max_val)(np.log10(x))
                     else:
                         processing_dict[feature] =  self.min_max_processing(min_val, max_val)
+
+                else:
+                    processing_dict[feature] = lambda x: x
                 
         return processing_dict
 
@@ -80,13 +90,14 @@ class OurOptimizer(AbstractOptimizer):
             function. Each suggestion is a dictionary where each key
             corresponds to a parameter being optimized.
         """
-        N = 5
-        x_guess = rs.suggest_dict([], [], self.api_config, n_suggestions=N*n_suggestions, random=self.random)
-        x_guess_p = [self.preprocessor(x) for x in x_guess]
+        N = 30
+        x_guess = rs.suggest_dict([], [], self.api_config, n_suggestions=N*n_suggestions,)# random=self.random)
+        x_guess_p = [self.process(x) for x in x_guess]
         y_guess = self.meta_model.predict(x_guess_p)
         best_idx = np.argsort(y_guess)[:n_suggestions]
         x_guess = [x_guess[i] for i in best_idx]
         return x_guess
+
 
     def observe(self, X, y):
         """Feed an observation back.
@@ -100,7 +111,8 @@ class OurOptimizer(AbstractOptimizer):
         y : array-like, shape (n,)
             Corresponding values where objective has been evaluated
         """
-        self.meta_model.fit(X, y)
+        X_p = [self.process(x) for x in X]
+        self.meta_model.fit(X_p, y)
 
 
 if __name__ == "__main__":
