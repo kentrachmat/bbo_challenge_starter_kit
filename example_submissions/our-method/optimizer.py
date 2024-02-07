090ad8ca49b67b787535a257d1ca80814b9b830b
Benedictus-Kent Rachmat <kentrachmat2313@gmail.com>
2024-02-07 19:28:41 +0100

    report

--- example_submissions/our-method/optimizer.py
+++ example_submissions/our-method/optimizer.py
@@ -29,9 +29,20 @@ class OurOptimizer(AbstractOptimizer):
         self.random = random
         self.meta_model = GaussianProcessRegressor(kernel=Matern(nu=2.5))
         self.api_config = api_config
+        self.X = np.empty((0, len(api_config)))  # Storing input points
+        self.y = np.empty((0, 1)) 
+
         self.preprocessor_dict = self.build_preprocessor(api_config)
 
     def process(self, x):
+        """Function to process one point in the hyperparameter space.
+
+        Args:
+            x (dict): Dictionary with values of hyperparameters.
+
+        Returns:
+            List: List of processed values.
+        """        
         features = []
         for feature in self.api_config:
             feature_values = x[feature]
@@ -39,6 +50,14 @@ class OurOptimizer(AbstractOptimizer):
         return features
 
     def build_preprocessor(self, api_config):
+        """This function builds a dictionary of functions to process the hyperparameters.
+
+        Args:
+            api_config (dict): Dictionary with the configuration of the hyperparameters.
+
+        Returns:
+            dict: Dictionary with the functions to process the hyperparameters.
+        """        
         processing_dict = {}
         for feature in api_config:
             if api_config[feature]['type'] == 'bool':
@@ -56,11 +75,9 @@ class OurOptimizer(AbstractOptimizer):
                 if 'range' in api_config[feature]:
                     min_val, max_val = api_config[feature]['range']
                     if api_config[feature]['space'] == 'log':
-                        min_val = np.log10(min_val)
-                        max_val = np.log10(max_val)
-                        processing_dict[feature] =  lambda x: self.min_max_processing(min_val, max_val)(np.log10(x))
+                        processing_dict[feature] =  self.log_min_max_processing(min_val, max_val)
                     else:
-                        processing_dict[feature] =  self.min_max_processing(min_val, max_val)
+                        processing_dict[feature] = self.min_max_processing(min_val, max_val)
 
                 else:
                     processing_dict[feature] = lambda x: x
@@ -68,13 +85,25 @@ class OurOptimizer(AbstractOptimizer):
         return processing_dict
 
     def categorical_processing(self, cats):
+        """This function returns a function to process categorical variables.
+
+        Args:
+            cats (list): List of possible values for the categorical variable.
+        """        
         def process(x):
             return cats.index(x)
         return process
     
     def min_max_processing(self, min_val, max_val):
+        """This function returns a function to process real variables."""
         def process(x):
             return (x - min_val) / (max_val - min_val)
+        return process
+    
+    def log_min_max_processing(self, min_val, max_val):
+        """This function returns a function to process real variables in log scale."""
+        def process(x):
+            return (np.log10(x) - np.log10(min_val)) / (np.log10(max_val) - np.log10(min_val))
         return process
 
     def suggest(self, n_suggestions=1):
@@ -92,37 +121,13 @@ class OurOptimizer(AbstractOptimizer):
             function. Each suggestion is a dictionary where each key
             corresponds to a parameter being optimized.
         """
-        # N = 30
-        # x_guess = rs.suggest_dict([], [], self.api_config, n_suggestions=N*n_suggestions,)# random=self.random)
-        # x_guess_p = [self.process(x) for x in x_guess]
-        # y_guess = self.meta_model.predict(x_guess_p)
-        # best_idx = np.argsort(y_guess)[:n_suggestions]
-        # x_guess = [x_guess[i] for i in best_idx]
-        # return x_guess
-        ################################
-        xi = 0.01  # Exploration-exploitation trade-off parameter
-        best_y = min(self.y, default=0)  # Best observed value
-
-        def acquisition_function(x):
-            x = np.array(x).reshape(1, -1)
-            mu, sigma = self.meta_model.predict(x, return_std=True)
-            z = (mu - best_y - xi) / sigma
-            return -(mu - best_y - xi) * norm.cdf(z) + sigma * norm.pdf(z)
-
-        # Initialize with random search
-        x_guess = rs.suggest_dict([], [], self.api_config, n_suggestions=n_suggestions)
-
-        # Optimize acquisition function
-        bounds = self.get_bounds()
-        for i in range(n_suggestions):
-            res = minimize(lambda x: -acquisition_function(x),
-                           x0=x_guess[i], bounds=bounds, method='L-BFGS-B')
-            x_guess[i] = res.x
-
-        # Convert back to dict format
-        x_guess_dicts = [self.convert_to_dict(x) for x in x_guess]
-        return x_guess_dicts
-
+        if len(self.X) > 0 and len(self.y) > 0:
+            # Sample new points by maximizing the expected improvement
+            next_guess = self.max_sampling(n_suggestions)
+        else:
+            # Use a random initialization strategy for the first suggestion
+            next_guess = rs.suggest_dict([], [], self.api_config, n_suggestions=n_suggestions,)
+        return next_guess
 
     def observe(self, X, y):
         """Feed an observation back.
@@ -136,36 +141,54 @@ class OurOptimizer(AbstractOptimizer):
         y : array-like, shape (n,)
             Corresponding values where objective has been evaluated
         """
-        X_p = [self.process(x) for x in X]
-        self.meta_model.fit(X_p, y)
+        # Process the input points and store them
+        X_p = np.array([self.process(x) for x in X])
+        # Store the input points and the corresponding values
+        self.X = np.vstack((self.X, X_p))
+        self.y = np.append(self.y, y)
 
-    def get_bounds(self):
-        bounds = []
-        for _, configs in self.api_config.items():
-            if configs['type'] in ['int', 'real']:
-                # For numerical types, use the provided range
-                bound = configs['range']
-            elif configs['type'] == 'bool':
-                # For boolean, the range is [0, 1]
-                bound = (0, 1)
-            elif configs['type'] == 'cat':
-                # For categorical, use index range
-                bound = (0, len(configs['values']) - 1)
-            bounds.append(bound)
-        return bounds
+        if self.X.shape[0] > 0 and self.y.shape[0] > 0:
+            # Fit a Gaussian Process model to the data
+            self.meta_model.fit(self.X, self.y)
+            
+    def max_sampling(self, n_suggestions):
+        N = 10 
+        M = 1000 # Number of random points to sample
+
+        # Sample M random points
+        candidates = rs.suggest_dict([], [], self.api_config, n_suggestions=max(M, N*n_suggestions))
+
+        # Process the input points
+        X = np.array([self.process(c) for c in candidates])
+
+        # Compute the expected improvement for each point
+        ei_list = self.expected_improvement(X)
+
+        # Select the n_suggestions points with the highest expected improvement
+        best = np.argsort(ei_list)[-n_suggestions:]
+        return [candidates[i] for i in best]
+
+    def expected_improvement(self, X):
+        """Expected improvement according to the formula in the paper.
+
+        Args:
+            X (array): Array with the values
+
+        Returns:
+            array: Array with the expected improvement for each point.
+        """        
+        mu, sigma = self.meta_model.predict(X, return_std=True)
+        mu_sample = self.meta_model.predict(self.X)
+
+        mu_sample_opt = np.max(mu_sample)
+
+        with np.errstate(divide='warn'):
+            gamma = (mu_sample_opt - mu)/sigma
+            ei = sigma*(gamma*norm.cdf(gamma) + norm.pdf(gamma))
+            ei[sigma == 0.0] = 0.0
+
+        return ei
     
-    def convert_to_dict(self, x_array):
-        x_dict = {}
-        for i, (feature, configs) in enumerate(self.api_config.items()):
-            value = x_array[i]
-            if configs['type'] == 'bool':
-                x_dict[feature] = bool(round(value))
-            elif configs['type'] == 'cat':
-                x_dict[feature] = configs['values'][int(round(value))]
-            elif configs['type'] in ['int', 'real']:
-                x_dict[feature] = value
-        return x_dict
-
 
 if __name__ == "__main__":
     experiment_main(OurOptimizer)
